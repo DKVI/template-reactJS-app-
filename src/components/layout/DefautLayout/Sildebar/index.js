eb09b6011dd8ce08eb13dae0a9bd89411529889a
DKVI <CT06N0164@actvn.edu.vn>
2023-03-20 21:09:30 +0700

    comit

--- src/components/layout/DefautLayout/Sildebar/index.js
+++ src/components/layout/DefautLayout/Sildebar/index.js
@@ -1,5 +1,14 @@
+import classNames from 'classnames/bind';
+import styles from './Sildebar.module.scss';
+
+const cx = classNames.bind(styles);
+
 function Sildebar() {
-    return <h2>Sildebar</h2>;
+    return (
+        <aside className={cx('wrapper')}>
+            <h2>Sildebar</h2>
+        </aside>
+    );
 }
 
 export default Sildebar;
