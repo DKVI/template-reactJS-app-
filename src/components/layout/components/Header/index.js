36ab2a57113424bca96a313c7c572d7536b07825
DKVI <CT06N0164@actvn.edu.vn>
2023-03-21 14:47:03 +0700

    submit

--- src/components/layout/components/Header/index.js
+++ src/components/layout/components/Header/index.js
@@ -1,13 +1,17 @@
 import styles from './Header.module.scss';
 import classNames from 'classnames/bind';
 const cx = classNames.bind(styles);
-console.log (styles)
+console.log(styles);
 function Header() {
-    return <header className={cx("wrapper")}>
-        <div className={cx("inner")}>
-
-        </div>
-    </header>;
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('inner')}>
+                <div className={cx('logo')}>
+                    
+                </div>
+            </div>
+        </header>
+    );
 }
 
 export default Header;
